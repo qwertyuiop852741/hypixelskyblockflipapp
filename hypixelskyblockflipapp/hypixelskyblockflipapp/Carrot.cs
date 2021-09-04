a20d84ad9fc8b4f863fd16c9ec32bde14da19206
qwertyuiop852741 <qwertyasdf741@naver.com>
2021-09-04 11:16:38 +0900

    ui 만듦 + json
    ui 당근 플립 부분 만듬, 코드

---
+++ hypixelskyblockflipapp/hypixelskyblockflipapp/Carrot.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hypixelskyblockflipapp
+{
+    public class Carrot
+    {
+        private int carrotprice; //일반당근
+        private int eCarrotprice; //인챈당근
+        private int gCarrotprice; //황금당근
+        private int carrotcandyupgrader; //당근사탕 업그레이드
+        public Carrot()
+        {
+            carrotcandyupgrader = 0;
+            carrotprice = 0;
+            eCarrotprice = 0;
+            gCarrotprice = 0;
+        }
+        public void Carrotpriceset()
+        {
+
+        }
+    }
+}
---
+++ hypixelskyblockflipapp/hypixelskyblockflipapp/Form1.Designer.cs
@@ -0,0 +1,253 @@
+﻿
+namespace hypixelskyblockflipapp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.carrotPage = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.lbProfit = new System.Windows.Forms.Label();
+            this.tabControl1.SuspendLayout();
+            this.carrotPage.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.carrotPage);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(3, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(445, 235);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // carrotPage
+            // 
+            this.carrotPage.Controls.Add(this.lbProfit);
+            this.carrotPage.Controls.Add(this.label6);
+            this.carrotPage.Controls.Add(this.label5);
+            this.carrotPage.Controls.Add(this.textBox5);
+            this.carrotPage.Controls.Add(this.button2);
+            this.carrotPage.Controls.Add(this.button1);
+            this.carrotPage.Controls.Add(this.label4);
+            this.carrotPage.Controls.Add(this.label3);
+            this.carrotPage.Controls.Add(this.label2);
+            this.carrotPage.Controls.Add(this.label1);
+            this.carrotPage.Controls.Add(this.textBox4);
+            this.carrotPage.Controls.Add(this.textBox3);
+            this.carrotPage.Controls.Add(this.textBox2);
+            this.carrotPage.Controls.Add(this.textBox1);
+            this.carrotPage.Location = new System.Drawing.Point(4, 22);
+            this.carrotPage.Name = "carrotPage";
+            this.carrotPage.Padding = new System.Windows.Forms.Padding(3);
+            this.carrotPage.Size = new System.Drawing.Size(437, 209);
+            this.carrotPage.TabIndex = 0;
+            this.carrotPage.Text = "당근";
+            this.carrotPage.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(793, 360);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(117, 32);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(108, 21);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(117, 59);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(108, 21);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(117, 86);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(108, 21);
+            this.textBox3.TabIndex = 2;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(117, 113);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(108, 21);
+            this.textBox4.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 12);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "당근 4608개";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(105, 12);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "인챈트당근 2560개";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(24, 86);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(87, 12);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "황금당근 192개";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(-2, 113);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(117, 12);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "캐럿캔디 업그레이드";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(59, 157);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(110, 27);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "가격설정";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(247, 158);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(119, 26);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "계산";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(240, 53);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(162, 21);
+            this.textBox5.TabIndex = 10;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(238, 32);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(179, 12);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "판매했을때 총가격(수수료 제외)";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(238, 86);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(103, 12);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "이익(수수료 제외)";
+            // 
+            // lbProfit
+            // 
+            this.lbProfit.AutoSize = true;
+            this.lbProfit.Location = new System.Drawing.Point(245, 113);
+            this.lbProfit.Name = "lbProfit";
+            this.lbProfit.Size = new System.Drawing.Size(9, 12);
+            this.lbProfit.TabIndex = 13;
+            this.lbProfit.Text = " ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(448, 248);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.carrotPage.ResumeLayout(false);
+            this.carrotPage.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage carrotPage;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lbProfit;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
